b40c5e8648d51a0921feba7f9cf342b407767abf
pardojer23 <mezeg39@gmail.com>
2019-10-28 14:22:39 -0400

    Updated Installation

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
 conda update --all
-conda export --no-builds >./RNAseqV2/envs/salmon.yml
 conda env create --file ./RNAseqV2/envs/salmon.yml
-conda export --no-builds >./RNAseqV2/envs/trimmomatic.yml
 conda env create --file ./RNAseqV2/envs/trimmomatic.yml
-conda export --no-builds >./RNAseqV2/envs/tximport.yml
 conda env create --file ./RNAseqV2/envs/tximport.yml
-conda export --no-builds >./RNAseqV2/envs/RNAseqV2.yml
 conda env create --file ./RNAseqV2/envs/RNAseqV2.yml
 
 source activate tximport
