8d557bfa7473b91b13fe5a8e83de9982a9e52d35
pardojer23 <mezeg39@gmail.com>
2019-10-30 08:07:31 -0400

    Updated Installation

--- RNAseqV2/R_Scripts/Install.r
+++ RNAseqV2/R_Scripts/Install.r
@@ -3,11 +3,11 @@
 # Created by: Jeremy Pardo
 # Created on: 2019-10-25
 if (!requireNamespace("BiocManager", quietly = TRUE))
-install.packages("BiocManager")
+install.packages("BiocManager", repos='http://cran.us.r-project.org')
 
 BiocManager::install("tximport")
 BiocManager::install("DESeq2")
-install.packages("readr")
-install.packages("dplyr")
-install.packages("stringr")
-install.packages("rjson")
+install.packages("readr", repos='http://cran.us.r-project.org')
+install.packages("dplyr", repos='http://cran.us.r-project.org')
+install.packages("stringr", repos='http://cran.us.r-project.org')
+install.packages("rjson", repos='http://cran.us.r-project.org')
