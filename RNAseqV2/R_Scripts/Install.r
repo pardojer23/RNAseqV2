0286e047e070de1aab9494785cc5e7fc3ff6c0e6
pardojer23 <mezeg39@gmail.com>
2019-10-30 10:45:28 -0400

    Updated Installation

--- RNAseqV2/R_Scripts/Install.r
+++ RNAseqV2/R_Scripts/Install.r
@@ -7,7 +7,17 @@ install.packages("BiocManager", repos='http://cran.us.r-project.org')
 
 BiocManager::install("tximport")
 BiocManager::install("DESeq2")
-install.packages("readr", repos='http://cran.us.r-project.org')
-install.packages("dplyr", repos='http://cran.us.r-project.org')
-install.packages("stringr", repos='http://cran.us.r-project.org')
-install.packages("rjson", repos='http://cran.us.r-project.org')
+# pkgTest function from Sacha Epskamp https://stackoverflow.com/users/567015/sacha-epskamp
+pkgTest <- function(x){
+    if (!require(x,character.only = TRUE))
+    {
+        install.packages(x, repos='http://cran.us.r-project.org')
+        if(!require(x,character.only = TRUE)) stop("Package not found")
+    }
+}
+
+pkgTest("readr")
+pkgTest("dplyr")
+pkgTest("stringr")
+pkgTest("rjson")
+
