cdc7215dbcd78a02c82a6520f47f912635bad6bb
pardojer23 <mezeg39@gmail.com>
2019-11-05 15:04:15 -0500

    Updated docstrings

--- RNAseqV2/RNAseqFunctions.py
+++ RNAseqV2/RNAseqFunctions.py
@@ -1,3 +1,24 @@
+"""
+Functions to conduct RNAseq data analysis.
+Copyright (C) 2019  Jeremy Pardo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see https://www.gnu.org/licenses/.
+
+See README.md for installation and execution instructions
+
+
+"""
 import subprocess
 import os
 class RNAseq_exp():
