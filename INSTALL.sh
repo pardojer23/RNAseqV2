7345af72d1de941b5f96f8fbc5648a199b9bc04e
pardojer23 <mezeg39@gmail.com>
2020-11-02 17:11:45 -0500

    updated to check if conda env exists

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,11 +2,25 @@
 
 conda update --all
 conda init bash
-conda env create --file ./RNAseqV2/envs/salmon.yml
-conda env create --file ./RNAseqV2/envs/trimmomatic.yml
-conda env create --file ./RNAseqV2/envs/fastp.yml
-conda env create --file ./RNAseqV2/envs/tximport.yml
-conda env create --file ./RNAseqV2/envs/RNAseqV2.yml
+
+
+function update_env {
+  if conda env list | grep -q $1; then
+     echo "updating the existing conda environment ${1}"
+     conda env update --prefix ./${1} \
+    --file ./RNAseqV2/envs/${1}.yml --prune
+  else
+    echo "creating a new conda environment ${1}"
+    conda env create --file ./RNAseqV2/envs/${1}.yml
+  fi
+}
+
+update_env tximport
+update_env salmon
+update_env trimmomatic
+update_env fastp
+update_env RNAseqV2
+
 
 FILE=~/.bashrc
 if [-f "$FILE"]; then
