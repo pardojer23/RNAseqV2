0ed816e702463742c8f1febae6ef82416e9bd12f
pardojer23 <mezeg39@gmail.com>
2020-11-10 10:11:45 -0500

    bug fixes

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,7 +7,7 @@ conda init bash
 function update_env {
   if conda env list | grep -q $1; then
      echo "updating the existing conda environment ${1}"
-     conda env update --prefix ./${1} \
+     conda env update --name ${1} \
     --file ./RNAseqV2/envs/${1}.yml --prune
   else
     echo "creating a new conda environment ${1}"
